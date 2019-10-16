65516a01d5298ec4b265220a492f9cec27ed1d58
Benjamin Brier <ben.brier14@gmail.com>
2019-10-16 12:51:47 -0400

    created function to find band time, location, and name

--- bands.js
+++ bands.js
@@ -9,4 +9,14 @@ function searchBand(userInput) {
     var band = userInput;
     var bandUrl = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp";
 
+    axios.get(bandUrl).then(
+        function(response) {
+            for (var i = 0; i <response.data.length; i++){
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " +response.data[i].venue.city);
+                console.log("Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
+                console.log("---------------------------------")
+            }
+        }
+    )
 }
