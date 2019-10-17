78465b2e7b2594f9679e6e12e95d7b63f3bc5e3b
Benjamin Brier <ben.brier14@gmail.com>
2019-10-17 11:29:31 -0400

    do-this, functioning

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 //Requirements needed to make page function
+var fs = require("fs");
 var searchBand = require("./bands.js");
 var searchMovie = require("./movies.js");
 var searchSpotify = require("./spotify.js");
@@ -13,21 +14,37 @@ var userInput = process.argv.splice(3, process.argv.length).join(' ');
 //this will be the switch commands that act as the backbone for Liri
 switch (userSearch) {
     case "help":
-        console.log("How may I assist you?" + 
-        "\nconcert-this: search for an artist's concert" + 
-        "\nmovie-this: search for information about a movie" + 
-        "\nspotify-this-song: search for information about a song" +
-        "\n-----------------------------");
+        console.log("How may I assist you?" +
+            "\nconcert-this: search for an artist's concert" +
+            "\nmovie-this: search for information about a movie" +
+            "\nspotify-this-song: search for information about a song" +
+            "\n-----------------------------");
         break;
     case "concert-this":
         searchBand(userInput);
         break;
-    case "spotify-this-song":
+    case "spotify-this":
         searchSpotify(userInput);
         break;
     case "movie-this":
         searchMovie(userInput);
         break;
+    case "do-this":
+        doThis();
+        break;
     default:
         console.log("Liri does not understand your input, type 'help' for instructions")
+}
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return err
+        }
+        var search = data.split(",");
+        if (search[0] === "spotify-this") {
+            var song = search[1].slice(1, -1);
+            searchSpotify(song)
+        }
+    })
 }
